52c847b5f7c5521fbd27e6b73bc944cb1db4eb16
Volat_BY <dingilevskiys@gmail.com>
2018-11-14 23:10:15 +0300

    Redux

--- src/index.js
+++ src/index.js
@@ -2,5 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import AuthExample from './App'
+import { Provider } from 'react-redux'
+import { store } from './components/Redux/Store'
 
-ReactDOM.render(<AuthExample />, document.getElementById('root'))
+ReactDOM.render(
+	<Provider store={store}>
+		<AuthExample />
+	</Provider>,
+	document.getElementById('root')
+)
